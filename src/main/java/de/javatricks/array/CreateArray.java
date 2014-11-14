502352fb3d3b9424d67164038ffa7f5a37d142bc
Thomas Lemmé <thomas.lemme@cgm.com>
2014-11-14 07:07:18 +0100

    Add Array things

---
+++ src/main/java/de/javatricks/array/CreateArray.java
@@ -0,0 +1,27 @@
+package de.javatricks.array;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomas18
+ * Date: 27.10.14
+ * Time: 08:17
+ * To change this template use File | Settings | File Templates.
+ */
+public class CreateArray {
+
+    public static void main(String[] args) {
+        byte[] byteArray = new byte[10];
+        short[] shortArray = new short[10];
+        int[] intArray = new int[10];
+        long[] longArray = new long[10];
+        float[] floatArray = new float[10];
+        double[] doubleArray = new double[10];
+        char[] charArray = new char[10];
+        boolean[] booleanArray = new boolean[10];
+        String[] beer = new String[]{"Zwickl", "Porter", "Weizen", "Pils"};
+        int[] numbers = new int[]{1, 2, 3, 4};
+
+        //beer = {"Zwickl", "Porter", "Weizen", "Pils"};
+    }
+
+}
--- src/main/java/de/javatricks/array/SortArray.java
+++ src/main/java/de/javatricks/array/SortArray.java
@@ -1,7 +1,5 @@
 package de.javatricks.array;
 
-import org.apache.commons.lang3.ArrayUtils;
-
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -11,47 +9,58 @@ import java.util.Comparator;
 public class SortArray {
 
     public static void main(String[] args) {
-        int[] intArray = {1,5,3,7,2,9};
-
+        int[] intArray = {1, 5, 3, 7, 2, 9};
         Arrays.sort(intArray);
-
         System.out.println(Arrays.toString(intArray));
 
-        Person[] persons = {
-                new Person("Tom", "Jones"),
-                new Person("James", "Brown"),
-                new Person("Carlos", "Santana")};
+        String[] stringArray = {"Gustav", "Ludwig", "Heinrich", "arnold"};
+        Arrays.sort(stringArray);
+        System.out.println(Arrays.toString(stringArray));
 
-        Arrays.sort(persons, new Comparator<Person>() {
+        stringArray = new String[]{"Gustav", "Ludwig", "Heinrich", "arnold"};
+        Arrays.sort(stringArray, new Comparator<String>() {
             @Override
-            public int compare(Person first, Person second) {
-                return first.getLastName().compareTo(second.getLastName());
+            public int compare(String first, String second) {
+                return first.toLowerCase().compareTo(second.toLowerCase());
             }
         });
-        System.out.println(Arrays.toString(persons));
+        System.out.println(Arrays.toString(stringArray));
 
-        Arrays.sort(persons, new Comparator<Person>() {
-            @Override
-            public int compare(Person first, Person second) {
-                return first.getFirstName().compareTo(second.getFirstName());
-            }
-        });
-        System.out.println(Arrays.toString(persons));
-
-        Arrays.sort(persons, (Person first, Person second)->first.getLastName().compareTo(second.getLastName()));
-        System.out.println(Arrays.toString(persons));
-
-        Arrays.sort(persons, (Person first, Person second)->first.getFirstName().compareTo(second.getFirstName()));
-        System.out.println(Arrays.toString(persons));
+//        Person[] persons = {
+//                new Person("Tom", "Jones"),
+//                new Person("James", "Brown"),
+//                new Person("Carlos", "Santana")};
+//
+//        Arrays.sort(persons, new Comparator<Person>() {
+//            @Override
+//            public int compare(Person first, Person second) {
+//                return first.getLastName().compareTo(second.getLastName());
+//            }
+//        });
+//        System.out.println(Arrays.toString(persons));
+//
+//        Arrays.sort(persons, new Comparator<Person>() {
+//            @Override
+//            public int compare(Person first, Person second) {
+//                return first.getFirstName().compareTo(second.getFirstName());
+//            }
+//        });
+//        System.out.println(Arrays.toString(persons));
+//
+//        Arrays.sort(persons, (Person first, Person second)->first.getLastName().compareTo(second.getLastName()));
+//        System.out.println(Arrays.toString(persons));
+//
+//        Arrays.sort(persons, (Person first, Person second)->first.getFirstName().compareTo(second.getFirstName()));
+//        System.out.println(Arrays.toString(persons));
     }
 
 }
 
-class Person{
+class Person {
     private String firstName;
     private String lastName;
 
-    public Person(String firstName, String lastName){
+    public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
